e048df880d6c38380f58f420d900ddf9e72f3d0e
Afolabi Olaoluwa <contact@afolabiolaluwa.com>
2018-05-06 01:27:44 +0100

    finished writing tests for model based on the limitation of the  csv

--- engines/csv_importer/app/models/csv_importer/user.rb
+++ engines/csv_importer/app/models/csv_importer/user.rb
@@ -1,6 +1,7 @@
 module CsvImporter
   class User < ApplicationRecord
+    before_save { self.email_address = email_address.downcase }
     validates :name, :email_address, presence: true
-    validates :email_address, uniqueness: true
+    validates :email_address, uniqueness: { case_sensitive: false }
   end
 end
--- engines/csv_importer/test/models/csv_importer/user_test.rb
+++ engines/csv_importer/test/models/csv_importer/user_test.rb
@@ -16,5 +16,29 @@ module CsvImporter
     test 'model should be valid' do
       assert @user.valid?
     end
+
+    test 'name should be present' do
+      @user.name = ''
+      assert_not @user.valid?
+    end
+
+    test 'email should be present' do
+      @user.email_address = ''
+      assert_not @user.valid?
+    end
+
+    test 'email addresses should be unique' do
+      duplicate_user = @user.dup
+      duplicate_user.email_address = @user.email_address.upcase
+      @user.save
+      assert_not duplicate_user.valid?
+    end
+
+    test 'email addresses should be saved as lower-case' do
+      mixed_case_email = 'Foo@ExAMPle.CoM'
+      @user.email_address = mixed_case_email
+      @user.save
+      assert_equal mixed_case_email.downcase, @user.reload.email_address
+    end
   end
 end
